d6b489e04585649881c99d7332c26044e5e2e7b1
Martin Connor <martinconnor@Martins-MacBook-Pro-3.local>
2019-07-01 15:39:16 -0700

    Check for vertical and horizontal TicTacToe solutions.

--- Desktop/t TicTacToe/client/App.jsx
+++ Desktop/t TicTacToe/client/App.jsx
@@ -7,7 +7,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       playersTurn: 1,
-      board: [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      board: [0, 0, 0, 0, 0, 0, 0, 0, 0], // 0, 3, 6 // 1, 4, 7
       gameOver: false,
     }
   }
@@ -36,6 +36,35 @@ class App extends React.Component {
       console.log('Oops, try going somewhere else!')
     }
 
+    if (this.state.playersTurn === 1) {
+      if (this.checkRows(newBoard, 1) === true) {
+        console.log('Congrats, player 1 wins on rows!')
+        this.setState({
+          gameOver: true,
+        })
+      };
+      if (this.checkVerticals(newBoard, 1) === true) {
+        console.log('Congrats, player 1 wins on verticals!')
+        this.setState({
+          gameOver: true,
+        })
+      };
+    } else {
+      console.log('here!');
+      if (this.checkRows(newBoard, 2) === true) {
+        console.log('Congrats, player 2 wins on rows!')
+        this.setState({
+          gameOver: true,
+        })
+      };
+      if (this.checkVerticals(newBoard, 2) === true) {
+        console.log('Congrats, player 2 wins on verticals!')
+        this.setState({
+          gameOver: true,
+        })
+      };
+    }
+
     this.setState({
       playersTurn: newPlayersTurn,
       board: newBoard,
@@ -44,12 +73,53 @@ class App extends React.Component {
 
   startNewGame(e) {
     e.preventDefault();
-    console.log('Starting new game!');
     this.setState({
       playersTurn: 1,
       board: newBoard,
       gameOver: false,
     })
+  }
+
+  checkRows(lastBoard, player) {
+    var threeInARow = 0;
+    var isSolved = false;
+  
+    for (var k = 0; k < 9; k += 3) {
+      for (var i = 0; i < 3; i++) {
+        console.log(`i: ${i}, k: ${k}`)
+        if (lastBoard[i + k] !== player) {
+          isSolved = false;
+        } else {
+          threeInARow++;
+          if (threeInARow === 3) {
+            return true;
+          }
+        }
+      }
+      threeInARow = 0;
+    };
+    return isSolved;
+  }
+
+  checkVerticals(lastBoard, player) {
+    var threeInARow = 0;
+    var isSolved = false;
+  
+    for (var i = 0; i < 3; i++) {
+      for (var k = 0; k < 9; k += 3) {
+        console.log(`i: ${i}, k: ${k}`)
+        if (lastBoard[i + k] !== player) {
+          isSolved = false;
+        } else {
+          threeInARow++;
+          if (threeInARow === 3) {
+            return true;
+          }
+        }
+      }
+      threeInARow = 0;
+    };
+    return isSolved;
   }
 
   render() {
